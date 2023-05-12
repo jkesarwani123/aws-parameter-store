3247047eaa2efebf3302a4d78a511df81909dfa3
Juhi K <jkesarwani.us@gmail.com>
2023-05-12 12:38:35 -0400

    add terra-parameter

--- main.tf
+++ main.tf
@@ -1,5 +1,6 @@
-resource "aws_ssm_parameter" "jkparameter2" {
+resource "aws_ssm_parameter" "jkparameter1" {
   name = "test.test2"
-  value = "jk123"
+  value = "jk11"
   type = "String"
+  key_id = "32feb83b-2783-4697-9ca6-dad07c97967b"
 }
