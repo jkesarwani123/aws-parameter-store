f0f6c2f05f8d98d83aecb8d4e2c3bfca66e142eb
Juhi K <jkesarwani.us@gmail.com>
2023-05-12 12:45:09 -0400

    add terra-parameter

--- main.tf
+++ main.tf
@@ -1,6 +1,7 @@
 resource "aws_ssm_parameter" "jkparameter1" {
-  name = "test.test2"
-  value = "jk11"
-  type = "String"
+  count=length(var.parameters)
+  name = var.parameters[count.index].name
+  value = var.parameters[count.index].value
+  type = "SecureString"
   key_id = "32feb83b-2783-4697-9ca6-dad07c97967b"
 }
