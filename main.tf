38f2d2ca5bb96515a359a48654dd2bf4b1124651
Juhi K <jkesarwani.us@gmail.com>
2023-05-12 09:43:19 -0400

    Ansible-parameter store

--- main.tf
+++ main.tf
@@ -1,11 +1,3 @@
-#resource "aws_ssm_parameter" "jkparameter-1" {
-  #count = length(var.parameters)
-  #name  = var.parameters[count.index].name
-  #type  = "String"
-  #value = var.parameters[count.index].value
-  #key_id = "32feb83b-2783-4697-9ca6-dad07c97967b"
-#}
-
 resource "aws_ssm_parameter" "jkparameter2" {
   name = "test.test2"
   value = "jk123"
