27ceb57fc266b9c6646a9e0a60f940fe9c1d461d
Juhi K <jkesarwani.us@gmail.com>
2023-05-11 20:50:46 -0400

    Ansible-parameter store

--- vars.tf
+++ vars.tf
@@ -1,1 +1,9 @@
 variable "parameters" {}
+parameters = {
+{ name = "frontend.catalog_url" , value = "http://catalogue.jkdevops.online:8080/"},
+  { name = "frontend.cart_url" , value = "http://cart.jkdevops.online:8080/"},
+  { name = "frontend.user_url" , value = "http://user.jkdevops.online:8080/"},
+  { name = "frontend.shipping_url" , value = "http://shipping.jkdevops.online:8080/"},
+  { name = "frontend.payment_url" , value = "http://payment.jkdevops.online:8080/"}
+
+}
