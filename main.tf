77c810383217923c68e8cde29865f33509ad1d4e
Juhi K <jkesarwani.us@gmail.com>
2023-05-13 14:58:15 -0400

    shipping ansible

--- main.tf
+++ main.tf
@@ -2,6 +2,6 @@ resource "aws_ssm_parameter" "jkparameter1" {
   count=length(var.parameters)
   name = var.parameters[count.index].name
   value = var.parameters[count.index].value
-  type = "String"
+  type = "SecureString"
   #key_id = "32feb83b-2783-4697-9ca6-dad07c97967b"
 }
