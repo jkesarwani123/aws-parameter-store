2a01b384aa0195f264962ac0c50b580d7ad8cc58
Juhi K <jkesarwani.us@gmail.com>
2023-05-16 09:04:21 -0400

    terraform ansible

--- main.tf
+++ main.tf
@@ -3,5 +3,5 @@ resource "aws_ssm_parameter" "jkparameter1" {
   name = var.parameters[count.index].name
   value = var.parameters[count.index].value
   type = "SecureString"
-  key_id = "32feb83b-2783-4697-9ca6-dad07c97967b"
+  key_id = "7a057889-68f1-4f4c-a3cb-4e0bfc8737c9"
 }
